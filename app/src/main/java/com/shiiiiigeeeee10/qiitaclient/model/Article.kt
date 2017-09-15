354f7658e40a3afe2e493838c4befaa032dfc655
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-15 13:38:07 +0900

    create models

---
+++ app/src/main/java/com/shiiiiigeeeee10/qiitaclient/model/Article.kt
@@ -0,0 +1,12 @@
+package com.shiiiiigeeeee10.qiitaclient.model
+
+/**
+ * Created by shigeru on 2017/09/15.
+ */
+
+data class Article(
+        val id: String,
+        val title: String,
+        val url: String,
+        val user: User
+)
---
+++ app/src/main/java/com/shiiiiigeeeee10/qiitaclient/model/User.kt
@@ -0,0 +1,11 @@
+package com.shiiiiigeeeee10.qiitaclient.model
+
+/**
+ * Created by shigeru on 2017/09/15.
+ */
+
+data class User(
+        val id: String,
+        val name: String,
+        val profileImageUrl: String
+)
